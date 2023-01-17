4f0a9d5127341048e1d77abe699bf908dfef1027
Maël Mandard <mael.mandard@laposte.net>
2023-01-17 16:26:47 +0100

    nettoyage data

--- Script/Projet.R
+++ Script/Projet.R
@@ -18,10 +18,12 @@ data[,"eqi_norm_corrupt"] #corruption normalisé
 data[,"eu_tour_nshotel"] #nuits passees dans hotel ou autre
 data[,"eu_rac_kil"] #victimes accidents de la route
 
-data <- data[,c("region_name", "year", "eqi_norm_eqi", "eu_d2jan_t", "eu_d3area_t", "eu_b5n_eur_hab", "eu_empl_edltotal",
+data <- data[,c("cname", "region_name", "year", "eqi_norm_eqi", "eu_d2jan_t", "eu_d3area_t", "eu_b5n_eur_hab", "eu_empl_edltotal",
                 "eu_hea_mdoc", "eqi_norm_corrupt", "eu_tour_nshotel", "eu_rac_kil")]
+
 nrow(data)
-summary(data)
+
+table(data$year)
 
 df <- data[,c("region_name", "year", "eu_d3area_t")]
 df <- df %>% filter(year == 2013)
@@ -33,8 +35,28 @@ data <- data %>% left_join(df,
 data <- data %>% select(region_name,year.x,eqi_norm_eqi,eu_d2jan_t,eu_d3area_t.y,eu_b5n_eur_hab,
                         eu_empl_edltotal,eu_hea_mdoc,eqi_norm_corrupt,eu_tour_nshotel,eu_rac_kil)
 
+summary(data)
 
 sum(is.na(data$eu_d3area_t.y))
 
-#data %>% filter(!is.na(data$eu_d3area_t.y))
+data <- data %>% filter(!is.na(data$eu_d3area_t.y)) %>%
+                select(-eu_hea_mdoc)
 
+table(data$year)
+
+
+data[is.na(data$eu_empl_edltotal),7] <- median(data$eu_empl_edltotal, na.rm = T)
+data[is.na(data$eu_tour_nshotel),9] <- median(data$eu_tour_nshotel, na.rm = T)
+
+summary(data)
+
+data <- na.omit(data)
+
+table <- data.frame(table(data$region_name))
+
+reg_sup <- table[table$Freq != 3,1]
+reg_sup
+
+data <- data[!(data$region_name %in% reg_sup),]
+
+table(data$year)
